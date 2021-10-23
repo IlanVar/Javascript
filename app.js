3acb7576aebeaf1b3e280d26a53706f26fa120d1
IlanVar <86364884+IlanVar@users.noreply.github.com>
2021-10-23 13:51:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ var x = innerWidth/2;
 var y = innerHeight/2;
 var particles = []
 
+function feedParticle(x,y) {
+    let min = 0; 
+let max = 20;
+for (let index = 0; index < 100; index++) {
+    particles.push([[x,y], [(Math.random() * (max - min) + min)/10-1, (Math.random() * (max - min) + min)/10-1], 10])            
+}
+}
 
 
 function draw() {
@@ -15,16 +22,10 @@ function draw() {
     requestAnimationFrame(draw);
     ctx.clearRect(0,0,innerWidth,innerHeight);
 
-    ctx.beginPath();
-    ctx.fillStyle = "#EDF5E1"
-    ctx.arc(x,y,10,0,2*Math.PI)
-    ctx.fill()
+    
 
     function draw_particle() {
-        let max = 0; 
-        let min = 20;
-        
-        particles.push([[x,y], [(Math.random() * (max - min) + min)/10-1, (Math.random() * (max - min) + min)/10-1], 10])
+
         particles.forEach(particle => {
             
             particle[0][0] += particle[1][0]
@@ -50,3 +51,10 @@ function draw() {
 }
 
 draw()
+
+canvas.addEventListener('click',function(e){
+    x = e.clientX
+    y = e.clientY
+    feedParticle(x,y)
+})
+
