374b911dd46ff39df3889dba5f32b839fb21a2a4
IlanVar <86364884+IlanVar@users.noreply.github.com>
2021-10-25 14:30:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -6,21 +6,18 @@ var ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-var initiateRocket = [innerWidth/2, innerHeight]
-var rocketFinish = false
-var rocketX = 0
-var rocketY = 0
-var vectorX = initiateRocket[0] 
-var vectorY = initiateRocket[1] 
 var x = 0;
 var y = 0;
 var particles = []
 var click = true
+var isDraw = false
+
+var color = 0
 
 function feedParticle(x,y,color) {
     let min = 0; 
 let max = 20;
-for (let index = 0; index < 150; index++) {
+for (let index = 0; index < 40; index++) {
     particles.push([[x,y], [(Math.random() * (max - min) + min)/10-1, (Math.random() * (max - min) + min)/10-1], 10, color])            
 }
 }
@@ -30,30 +27,14 @@ function draw() {
     requestAnimationFrame(draw);
     ctx.clearRect(0,0,innerWidth,innerHeight);
 
-    function rocket() { 
-
-        ctx.beginPath();
-        ctx.fillStyle = `hsl(180, 80%, 70%)`
-        ctx.arc(vectorX,vectorY,10,0,2*Math.PI)
-        ctx.fill()
-
-        if (vectorY<=rocketY){
-            rocketFinish = true
-        }
-        if(rocketX != 0 && rocketY != 0){
-            vectorX+= (rocketX-initiateRocket[0])/25
-            vectorY+= (rocketY-initiateRocket[1])/25
-        }
-    }
-
     function draw_particle() {
 
         particles.forEach(particle => {
             
-            particle[0][0] += particle[1][0]*2.3
-            particle[0][1] += particle[1][1]*2.5
+            particle[0][0] += particle[1][0]*2
+            particle[0][1] += particle[1][1]*2
             particle[1][1] += 0.03
-            particle[2] -= 0.2
+            particle[2] -= 0.1
 
             if (particle[2] >= 0) {
             ctx.beginPath();
@@ -70,41 +51,38 @@ function draw() {
         }); 
 
     }
+
+    draw_particle()
     
-    canvas.addEventListener('click',function(e){
-        click = false
-        rocketX = e.clientX
-        rocketY = e.clientY
-        rocketFinish=false
-        vectorX = initiateRocket[0] 
-        vectorY = initiateRocket[1] 
 
-    })
-
-    if(rocketFinish==false) {
-    rocket()
-    }
-
-    if (rocketFinish == true) {
-        draw_particle()
-    }
+  
 
 
-    if (click == true) {
-        ctx.font = '50px arial';
-        ctx.fillStyle = "#EDF5E1";
-        ctx.fillText("CLICK", (innerWidth/2)-50,(innerHeight/2)-25);
-    }
-
+if (click == true) {
+    ctx.font = '50px arial';
+    ctx.fillStyle = "#EDF5E1";
+    ctx.fillText("CLICK", (innerWidth/2)-50,(innerHeight/2)-25);
+}
 }
 
 draw()
 
-canvas.addEventListener('click',function(e){
+
+
+
+canvas.addEventListener('mousedown',function(){
+    isDraw = true
+    canvas.addEventListener('mousemove',function(e){
     click = false
-    let x = e.clientX
-    let y = e.clientY
-    let color = (Math.random() * (360 - 0) + 0)
-    feedParticle(x,y,color)
+    x = e.clientX
+    y = e.clientY
+    color ++
+    if (isDraw == true){
+    feedParticle(x,y,color)}
+});
+})
+
+canvas.addEventListener('mouseup',function() {
+    isDraw = false
 })
 
